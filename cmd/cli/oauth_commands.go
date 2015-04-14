9085ffa106bb4029ca18cbf944b924e0706fef9e
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2015-04-14 13:57:00 +0200

    add a command to create a new provider

--- cmd/cli/oauth_commands.go
+++ cmd/cli/oauth_commands.go
@@ -22,6 +22,15 @@ var oauthList = &cobra.Command{
 	},
 }
 
+var oauthPut = &cobra.Command{
+	Use:   "put [# network] [# clientid] [# clientsecret]",
+	Short: "add a new oauth provider with the given data",
+	Long:  "some predefined networks only require the clientid/secret, others need more data so use the flags to add more data",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
 func init() {
-	oauth.AddCommand(oauthList)
+	oauth.AddCommand(oauthList, oauthPut)
 }
